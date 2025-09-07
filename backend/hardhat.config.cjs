fbce363e1a41197fc5067a50e4499b2b7c86bce6
Namoj-design <namojperiakumar@gmail.com>
2025-09-08 00:25:58 +0530

    hardhat add

---
+++ backend/hardhat.config.cjs
@@ -0,0 +1,9 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+export default  {
+  solidity: "0.8.20",
+  paths: {
+    artifacts: "./artifacts",
+    sources: "./contracts",
+  },
+};
--- backend/hardhat.config.js
+++
@@ -1,9 +0,0 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-module.exports = {
-  solidity: "0.8.20",
-  paths: {
-    artifacts: "./artifacts",
-    sources: "./contracts",
-  },
-};
